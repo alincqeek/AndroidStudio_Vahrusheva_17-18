3f1223f2d65b9eb0ecd7428262c432af4e34631a
alincqeek <a72658695@gmail.com>
2026-03-05 20:39:43 +0300

    Step 3: Create WeatherData model

---
+++ app/src/main/java/com/arlina/androidstudio_vahrusheva_17_18/data/WeatherData.kt
@@ -0,0 +1,9 @@
+package com.arlina.androidstudio_vahrusheva_17_18.data
+
+data class WeatherData(
+    val temperature: Int? = null,
+    val humidity: Int? = null,
+    val windSpeed: Int? = null,
+    val isLoading: Boolean = false,
+    var error: String? = null,
+)
